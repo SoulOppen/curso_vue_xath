1e198e82f53a87aaae909674d21786e54c95c083
SoulOppen <arieloppenheimer@outlook.com>
2024-07-26 18:54:54 -0400

    add:contactos y paginas

---
+++ assets/js/paginas.js
@@ -0,0 +1,11 @@
+const paginas = [
+  "Página principal",
+  "Preguntas frecuentes",
+  "Manuales y tutoriales",
+  "Noticias",
+  "Calendario",
+  "Perfil de usuario",
+  "Organigrama",
+  "Chat",
+];
+export default paginas;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 import personas from "./personas.js";
 import conversaciones from "./conversaciones.js";
-console.log(personas, conversaciones);
+import paginas from "./paginas.js";
+
+const $contact = document.getElementById("contact_list");
+const $nav = document.getElementById("nav_list");
+$contact.innerHTML = personas
+  .map((persona) => {
+    return `<li><a class="dropdown-item" href="#">${persona.nombre}</a></li>`;
+  })
+  .join("");
+$nav.innerHTML = paginas
+  .map((pagina) => {
+    if (pagina.includes("Chat")) {
+      return `<li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">${pagina}</a>
+              </li>`;
+    }
+    return `<li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">${pagina}</a>
+              </li>`;
+  })
+  .join("");
