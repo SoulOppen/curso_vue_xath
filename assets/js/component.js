bc7fbf937f6412b1e5f15c14c418fc60de4ff988
SoulOppen <arieloppenheimer@outlook.com>
2024-07-27 23:56:25 -0400

    add:style area contacto y card contacto en modo grande

--- assets/js/component.js
+++ assets/js/component.js
@@ -1,16 +1,21 @@
 const personaDropDown = (persona) =>
-  `<li><div>
-<img src="${persona.foto}" alt="${persona.nombre} ${
+  `<li class="m-2"><div class="d-flex">
+<div><img src="${persona.foto}" alt="${persona.nombre} ${
     persona.apellido
-  }" class="img">
+  }" class="img"></div>
 <div>${persona.sobrenombre ?? persona.nombre + " " + persona.apellido}</div>
 </div></li>`;
 const chatPersona = (persona) =>
-  `<li><div>
-<img src="${persona.foto}" alt="${persona.nombre} ${
+  `<li class="g-0"><div class="contactoBig">
+    <img src="${persona.foto}" alt="${persona.nombre} ${
     persona.apellido
-  }" class="img">
-<div>${persona.sobrenombre ?? persona.nombre + " " + persona.apellido}</div>
+  }" class="contactoBig__img rounded-circle">
+  <div class="mx-4 contactoBig__main">
+<div class="contactoBig__nombre">${
+    persona.sobrenombre ?? persona.nombre + " " + persona.apellido
+  }</div>
+  <div class="contactoBig__fecha">15/6/2024 15:54</div>
+  </div>
 </div></li>`;
 const pagina = (pagina, active = false) => {
   if (active) {
